58d27de1e35cef13a5d72ac310570c273de59f5d
Retno Ekayanti <80891391+reykayanti@users.noreply.github.com>
2021-09-01 22:07:54 +0700

    Upload PHP
    PHP ini diletakkan di dalam folder server yang dipakai contohnya XAMPP

---
+++ MP/hapusPgw.php
@@ -0,0 +1,29 @@
+<?php
+ 
+ 
+ /*
+ 
+ penulis: Muhammad yusuf
+ website: https://www.kodingindonesia.com/
+ 
+ */
+ 
+ //Mendapatkan Nilai ID
+ $id = $_GET['id'];
+ 
+ //Import File Koneksi Database
+ require_once('koneksi.php');
+ 
+ //Membuat SQL Query
+ $sql = "DELETE FROM tb_pegawai WHERE id=$id;";
+ 
+ 
+ //Menghapus Nilai pada Database 
+ if(mysqli_query($con,$sql)){
+ echo 'Berhasil Menghapus Pegawai';
+ }else{
+ echo 'Gagal Menghapus Pegawai';
+ }
+ 
+ mysqli_close($con);
+ ?>
